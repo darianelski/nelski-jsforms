d1dd60a6f7e393659b882b4e55e95c8fa55cbba1
Daria Nelski <daria.nelski@gmail.com>
2018-08-28 13:10:54 +0300

    first commit

--- js/check-comment.js
+++ js/check-comment.js
@@ -1,6 +1,60 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
-	console.log('Hello world!');
+	(function(){
+		var commentFormCheck = {
+
+			isValid: true,
+
+			init: function() {
+				//вызов внутренних функций
+				this._setUpListeners(); //прослушка событий
+			},
+
+			_setUpListeners: function() {
+				$('#comment-form').on('submit', commentFormCheck._validateForm).on('submit', commentFormCheck._sendEmail);	
+			},
+
+			// Проверка валидации
+			_validateForm: function(event){
+				event.preventDefault();
+				console.log('_validateForm runs');
+
+				var form = $(this),
+						inputs = form.find('textarea'),
+						valid = true;
+
+				// проходим по каждому input
+				$.each(inputs, function(index, val){
+					var input = $(val),
+							value = input.val().trim(),
+							textError = $('#textError');
+
+					if ( value.length === 0 ){
+						// показать ошибку, если сообщение пустое
+						textError.fadeIn(500);
+						valid = false;
+					}
+
+						// скрыть ошибку по набору текста
+						input.on('keydown', function(){
+							textError.fadeOut();
+						});
+				});
+
+				commentFormCheck.isValid = valid;
+			},
+
+			// Перенаправление при успешной отправке
+			_sendEmail: function(){
+				console.log('commentFormCheck.isValid = '+ commentFormCheck.isValid);
+				if (commentFormCheck.isValid === true) {
+					window.location.href = 'success.html';
+				} 
+			},
+		};
+
+	commentFormCheck.init();
+
+	}());
 
 });
-
